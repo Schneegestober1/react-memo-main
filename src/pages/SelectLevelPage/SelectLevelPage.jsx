4ad2573e7eca35d86d9089cbb78e52dc9702ac80
Kholov Rustam <rustamkholow5@gmail.com>
2024-06-09 00:51:12 +0500

    first commit

---
+++ src/pages/SelectLevelPage/SelectLevelPage.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import styles from "./SelectLevelPage.module.css";
+
+export function SelectLevelPage() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.modal}>
+        <h1 className={styles.title}>Выбери сложность</h1>
+        <ul className={styles.levels}>
+          <li className={styles.level}>
+            <Link className={styles.levelLink} to="/game/3">
+              1
+            </Link>
+          </li>
+          <li className={styles.level}>
+            <Link className={styles.levelLink} to="/game/6">
+              2
+            </Link>
+          </li>
+          <li className={styles.level}>
+            <Link className={styles.levelLink} to="/game/9">
+              3
+            </Link>
+          </li>
+        </ul>
+        <div className={styles.wrap}>
+          <h3 className={styles.subtitle}>Дополнительные попытки</h3>
+          <input type="checkbox" />
+        </div>
+      </div>
+    </div>
+  );
+}
