f18d756484ef142b423475922af8f8d930e7c9f1
Kholov Rustam <rustamkholow5@gmail.com>
2024-06-25 22:41:09 +0500

    const achivImg1 = achievements.includes(1) ? vectorActiveImg : vectorNotActiveImg;   const achivImg2 = achievements.includes(2) ? magicBallActiveImg : magicBallNotActiveImg;

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -7,6 +7,7 @@ import { Button } from "../../components/Button/Button";
 import { Card } from "../../components/Card/Card";
 import { LivesContext } from "../../context/livesContext";
 import { calcUnits } from "../../utils/helpers";
+import alahamoraImg from "./img/alohomora.png";
 
 // Игра закончилась
 const STATUS_LOST = "STATUS_LOST";
@@ -226,6 +227,30 @@ export function Cards({ pairsCount = 3, previewSeconds = 5 }) {
     };
   }, [gameStartDate, gameEndDate]);
 
+  const [isActivAlohomora, setIsActivAlohomora] = useState(false);
+
+  const onAlohomora = () => {
+    setIsActivAlohomora(true);
+    const firstCard = cards.find(card => card.open === false);
+    const newCards = cards.map(card => {
+      return card.suit === firstCard.suit && card.rank === firstCard.rank ? { ...card, open: true } : card;
+    });
+    const isGameOver = newCards.some(item => !item.open);
+    setCards(newCards);
+    if (!isGameOver) {
+      finishGame(STATUS_WON);
+    }
+  };
+
+  const achievements = [];
+
+  if (!lives === 3) {
+    achievements.push(1);
+  }
+  if (!isActivAlohomora) {
+    achievements.push(2);
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -249,6 +274,27 @@ export function Cards({ pairsCount = 3, previewSeconds = 5 }) {
             </>
           )}
         </div>
+        {status === STATUS_PREVIEW ? (
+          ""
+        ) : (
+          <div className={styles.superPowersContainer}>
+            <div className={styles.wrapper}>
+              <img
+                onClick={onAlohomora}
+                className={isActivAlohomora ? styles.disabledEpiphany : styles.superPowerImg}
+                src={alahamoraImg}
+                alt=""
+              />
+              <div className={styles.layout}></div>
+              <div className={styles.bubble}>
+                <h4 className={styles.title}>Алохомора</h4>
+                <p className={styles.description}>Открывается случайная пара карт.</p>
+              </div>
+            </div>
+            <div className={styles.layout}></div>
+          </div>
+        )}
+
         {status === STATUS_IN_PROGRESS ? <Button onClick={resetGame}>Начать заново</Button> : null}
       </div>
 
@@ -279,7 +325,7 @@ export function Cards({ pairsCount = 3, previewSeconds = 5 }) {
             onClick={resetGame}
             pairsCount={pairsCount}
             timer={timer}
-            achievements={null}
+            achievements={achievements}
           />
         </div>
       ) : null}
--- src/components/Leaderboard/Leaderboard.jsx
+++ src/components/Leaderboard/Leaderboard.jsx
@@ -1,20 +1,37 @@
 import styles from "./Leaderboard.module.css";
-// import magicBallActiveImg from "./images/magic-ball-active.png";
-// import magicBallNotActiveImg from "./images/magic-ball-not-active.png";
-// import vectorActiveImg from "./images/vector-active.png";
-// import vectorNotActiveImg from "./images/vector-not-active.png";
-// import hardModeImg from "./images/hard-mode.png";
-// import superPowersImg from "./images/super-powers.png";
+import magicBallActiveImg from "./images/magic-ball-active.png";
+import magicBallNotActiveImg from "./images/magic-ball-not-active.png";
+import vectorActiveImg from "./images/vector-active.png";
+import vectorNotActiveImg from "./images/vector-not-active.png";
+import hardModeImg from "./images/hard-mode.png";
+import superPowersImg from "./images/super-powers.png";
 
 export const Leaderboard = ({ position, user, time, achievements }) => {
-  // const achivImg1 = achievements.includes(1) ? vectorActiveImg : vectorNotActiveImg;
-  // const achivImg2 = achievements.includes(2) ? magicBallActiveImg : magicBallNotActiveImg;
+  const achivImg1 = achievements.includes(1) ? vectorActiveImg : vectorNotActiveImg;
+  const achivImg2 = achievements.includes(2) ? magicBallActiveImg : magicBallNotActiveImg;
   return (
     <div>
       <li className={styles.item}>
         <span className={styles.position}>{position}</span>
         <span className={styles.user}>{user}</span>
-        <span className={styles.img}>1</span>
+        <div className={styles.img}>
+          <div className={achivImg1 === vectorActiveImg ? styles.wrapper : ""}>
+            <div>
+              <img src={achivImg1} alt="#" />
+            </div>
+            <div className={styles.bubble}>
+              <img className={styles.description} src={hardModeImg} alt="#" />
+            </div>
+          </div>
+          <div className={achivImg2 === magicBallActiveImg ? styles.wrapper : ""}>
+            <div>
+              <img src={achivImg2} alt="#" />
+            </div>
+            <div className={styles.bubble}>
+              <img className={styles.description} src={superPowersImg} alt="#" />
+            </div>
+          </div>
+        </div>
         <span className={styles.time}>{time}</span>
       </li>
     </div>
